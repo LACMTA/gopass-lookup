711b88392a121a1f10ff03972ab08ea1f516259c
Nina Kin <matikin9@gmail.com>
2022-08-03 23:29:32 -0700

    uswds installed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/js");
+	eleventyConfig.addPassthroughCopy("assets/uswds");
 
 	return {
 		pathPrefix: "/gopass-lookup/",
