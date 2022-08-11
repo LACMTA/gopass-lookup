a8cca6f104eb35273e5c17100c1a5e62b70bae95
Nina Kin <matikin9@gmail.com>
2022-08-11 12:41:11 -0700

    up/down arrows navigate through suggestion list

--- src/js/site.js
+++ src/js/site.js
@@ -2,55 +2,77 @@ const SCHOOLS_JSON = "data/schools.json";
 
 let SCHOOLS_DATA = [];
 
+loadDataFromJSON(SCHOOLS_JSON);
+
+document.addEventListener('click', clickOutsideSearchInput);
+
+window.onload = (e) => {
+	let search_field = document.getElementById('search-field');
+	search_field.addEventListener('click', showSuggestionList);
+	search_field.addEventListener('input', function() {
+		loadSuggestedSchools(SCHOOLS_DATA);
+	});
+
+	let search_suggestions = document.getElementById('search-suggestions');
+
+	let search_height = search_field.offsetHeight;
+	let search_width = search_field.offsetWidth;
+
+	search_suggestions.style.top = search_height + 'px';
+	search_suggestions.style.width = search_width + 'px';
+
+	document.getElementById('search-button').addEventListener('click', clickSearchButton);
+};
+
 function loadDataFromJSON(file) {
 	fetch(file)
-	.then(response => response.json())
-	.then(data => {
-		SCHOOLS_DATA = data;
-		document.getElementById('search-field').addEventListener('keyup', function() {
-			loadSuggestedSchools(SCHOOLS_DATA);
+		.then(response => response.json())
+		.then(data => {
+			SCHOOLS_DATA = data;
+			document.getElementById('search-field').addEventListener('keydown', function (e) {
+				navigateSuggestionList(e);
+			});
+			document.getElementById('search-field').addEventListener('click', function () {
+				loadSuggestedSchools(SCHOOLS_DATA);
+			});
 		});
-		document.getElementById('search-field').addEventListener('click', function() {
-			loadSuggestedSchools(SCHOOLS_DATA);
-		});
-	});
 }
 
 function loadSuggestedSchools(school_list) {
 	input = document.getElementById('search-field').value;
-	console.log(input);
 
-	let search_results = fuzzysort.go(input, school_list, {
-		key: ['school_name_with_some_districts_attached'],
-		limit: 5,
-		threshold: -10000
-	});
-	let search_suggestions = document.getElementById('search-suggestions');
-	let search_suggestions_list = document.getElementById('search-suggestions-list');
-	search_suggestions_list.innerHTML = '';
-
-	if (search_results.length == 0 && input.length != 0) {
-		console.log('no results');
-		let no_results = document.createElement('li');
-		no_results.innerHTML = '<em>School not found? Let us know!</em>';
-		no_results.onclick = (e) => {
-			let search_button = document.getElementById('search-button');
-			search_button.click();
-		};
-
-		search_suggestions_list.appendChild(no_results);
-	} else {
-		search_results.forEach(element => {
-			let list_item = document.createElement('li');
-
-			list_item.innerHTML = fuzzysort.highlight(fuzzysort.single(input, element.target), '<strong>', '</strong>');
-			list_item.setAttribute('data-id', element.obj.index);
-			list_item.setAttribute('data-gopass', element.obj.participating);
-			list_item.addEventListener('click', clickSuggestionList);
-
-			search_suggestions_list.appendChild(list_item);
-			search_suggestions.style.display = 'block';
+	if (input != '') {
+		let search_results = fuzzysort.go(input, school_list, {
+			key: ['school_name_with_some_districts_attached'],
+			limit: 5,
+			threshold: -10000
 		});
+		let search_suggestions = document.getElementById('search-suggestions');
+		let search_suggestions_list = document.getElementById('search-suggestions-list');
+		search_suggestions_list.innerHTML = '';
+
+		if (search_results.length == 0 && input.length != 0) {
+			let no_results = document.createElement('li');
+			no_results.innerHTML = '<em>School not found? Let us know!</em>';
+			no_results.onclick = (e) => {
+				let search_button = document.getElementById('search-button');
+				search_button.click();
+			};
+
+			search_suggestions_list.appendChild(no_results);
+		} else {
+			search_results.forEach(element => {
+				let list_item = document.createElement('li');
+
+				list_item.innerHTML = fuzzysort.highlight(fuzzysort.single(input, element.target), '<strong>', '</strong>');
+				list_item.setAttribute('data-id', element.obj.index);
+				list_item.setAttribute('data-gopass', element.obj.participating);
+				list_item.addEventListener('click', clickSuggestionList);
+
+				search_suggestions_list.appendChild(list_item);
+				search_suggestions.style.display = 'block';
+			});
+		}
 	}
 }
 
@@ -65,11 +87,59 @@ function clickSuggestionList(e) {
 	search_button.click();
 }
 
-function showSuggestionsList() {
+function showSuggestionList() {
 	let search_suggestions = document.getElementById('search-suggestions');
 	let search_suggestions_list = document.getElementById('search-suggestions-list');
 	if (search_suggestions_list.childElementCount > 0) {
 		search_suggestions.style.display = 'block';
+	}
+}
+
+function navigateSuggestionList(event) {
+	console.log(event.target);
+	let search_suggestions = document.getElementById('search-suggestions');
+	let suggestion_list_items = document.querySelectorAll('#search-suggestions-list > li');
+	let active_suggestion = document.querySelector('#search-suggestions-list > li.active');
+	// up: 38
+	// down: 40
+	console.log(search_suggestions.querySelector('li'));
+	console.log(search_suggestions.querySelector('li').classList);
+	console.log(search_suggestions.querySelector('li').classList.contains('active'));
+	console.log('list items: ' + suggestion_list_items[0].classList);
+
+	if (isVisible(search_suggestions)) {
+		switch (event.keyCode) {
+			case 38:
+				if (active_suggestion != null) {
+					active_suggestion.classList.toggle('active');
+					if (active_suggestion.previousElementSibling != null) {
+						active_suggestion.previousElementSibling.classList.toggle('active');
+					} else {
+						suggestion_list_items[suggestion_list_items.length - 1].classList.toggle('active');
+					}
+				} else {
+					suggestion_list_items[suggestion_list_items.length - 1].classList.toggle('active');
+				}
+				break;
+			case 40:
+				if (active_suggestion != null) {
+					active_suggestion.classList.toggle('active');
+
+					if (active_suggestion.nextElementSibling != null) {
+						active_suggestion.nextElementSibling.classList.toggle('active');
+					} else {
+						suggestion_list_items[0].classList.toggle('active');
+					}
+				} else {
+					suggestion_list_items[0].classList.toggle('active');
+				}
+				break;
+			case 13:
+				if (active_suggestion != null) {
+					active_suggestion.click();
+				}
+				break;
+		}
 	}
 }
 
@@ -86,27 +156,8 @@ function clickSearchButton() {
 	}
 }
 
-window.onload = (e) => {
-	let search_field = document.getElementById('search-field');
-	let search_suggestions = document.getElementById('search-suggestions');
 
-	let search_height = search_field.offsetHeight;
-	let search_width = search_field.offsetWidth;
-
-	search_field.addEventListener('click', showSuggestionsList);
-
-	document.getElementById('search-button').addEventListener('click', clickSearchButton);
-	
-	document.addEventListener('click', clickOutsideSearchInput);
-
-	search_suggestions.style.top = search_height + 'px';
-	search_suggestions.style.width = search_width + 'px';
-
-	loadDataFromJSON(SCHOOLS_JSON);
-	// loadDataFromCSV(MERGED_SCHOOLS_CSV);
-};
-
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
 	adjustSuggestionListWidth();
 });
 
@@ -114,7 +165,6 @@ function adjustSuggestionListWidth() {
 	let search_field = document.getElementById('search-field');
 	let search_suggestions = document.getElementById('search-suggestions');
 	search_suggestions.style.width = search_field.offsetWidth + 'px';
-	console.log('search-field width: ' + document.getElementById('search-field').offsetWidth);
 }
 
 function clickOutsideSearchInput(e) {
@@ -126,4 +176,4 @@ function clickOutsideSearchInput(e) {
 	}
 }
 
-const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length ); 
+const isVisible = elem => !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length); 
