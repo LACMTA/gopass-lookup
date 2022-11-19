4ed179463f9ff309204585730f8d457468e59933
Nina Kin <matikin9@gmail.com>
2022-11-18 19:18:05 -0800

    clean up data files, show aggregated phone numbers, remove dupes from school suggestions

--- .eleventy.js
+++ .eleventy.js
@@ -3,15 +3,35 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/js");
 	eleventyConfig.addPassthroughCopy("src/img");
 	eleventyConfig.addPassthroughCopy("assets/uswds");
-	eleventyConfig.addPassthroughCopy("src/data");
+	eleventyConfig.addPassthroughCopy("src/_data");
 	eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+	// check for empty values befor using this filter
+	eleventyConfig.addLiquidFilter('separateByPipe', function(value) {
+		let value_arr = value.split('|');
+		let result = '';
+
+		if (value_arr.length > 1) {
+			value_arr.forEach((val, key, arr) => {
+				if (Object.is(arr.length - 1, key)) {
+					result += 'or ' + val.trim();
+				} else {
+					result += val.trim() + ', ';
+				}
+			});
+		} else {
+			result = value_arr[0];
+		}
+
+		return result;
+	});
 
 	return {
 		pathPrefix: "/gopass-lookup/",
 		dir: {
 			input: "src",
 			output: "docs",
-			data: "data"
+			data: "_data"
 		}
 	};
 };
